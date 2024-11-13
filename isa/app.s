db289f9a864c82d4d9574837fc9fc99a44c1be50
Bogdan Nikitin <nikitin.bogdan333@yandex.ru>
2024-11-13 14:38:23 +0300

    Extract instruction

--- isa/app.s
+++ isa/app.s
@@ -1,3 +1,4 @@
+entry
     alloc x1 262144 
     mov x4 x1 
     mov x2 0 
@@ -8,8 +9,8 @@ x_loop_init
     and x6 1
     store x6 x4
     add x4 1
-    loop x3 512 x_loop_init
-    loop x2 256 y_loop_init
+    loop x_loop_init x3 512 
+    loop y_loop_init x2 256 
     mov x2 0 
 main_loop
     mov x3 x2
@@ -24,8 +25,8 @@ x_loop_draw
     movifz x10 x12 0xFF000000 0xFFFFFFFF 
     putpixel x5 x4 x12
     add x6 1
-    loop x5 512 x_loop_draw
-    loop x4 256 y_loop_draw
+    loop x_loop_draw x5 512 
+    loop y_loop_draw x4 256 
     flush
     movifz x2 x7 131072 0 
     mov x9 x7
@@ -44,19 +45,17 @@ dy_loop
     shl x12 9 
     and x12 130560 
 dx_loop
-    brz x13 x10 loop_end
-    mov x14 x13
-    add x14 x5 
-    and x14 511 
+    brz loop_end x13 x10 
+    andd x14 x13 x5 511 
     thor x14 x12 x3
     add x14 x1 
     load x15 x14 
     add x11 x15 
 loop_end
-    loop x13 2 dx_loop
-    loop x10 2 dy_loop
-    breq x11 3 live
-    breq x11 2 remain
+    loop dx_loop x13 2 
+    loop dy_loop x10 2 
+    breq live x11 3 
+    breq remain x11 2 
     mov x10 0
     br update
 live
@@ -68,7 +67,7 @@ update
     store x10 x16 
     add x8 1
     add x9 1
-    loop x5 512 x_loop_main
-    loop x4 256 y_loop_main
+    loop x_loop_main x5 512 
+    loop y_loop_main x4 256 
     xor x2 1 
     br main_loop
