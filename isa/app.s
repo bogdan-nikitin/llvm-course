4bd505608563ea2ddbed7c767a59e85c615ad6f9
Bogdan Nikitin <nikitin.bogdan333@yandex.ru>
2024-11-13 14:46:16 +0300

    Uppercase for instructions

--- isa/app.s
+++ isa/app.s
@@ -1,73 +1,73 @@
 entry
-    alloc x1 262144 
-    mov x4 x1 
-    mov x2 0 
+    ALLOC x1 262144 
+    MOV x4 x1 
+    MOV x2 0 
 y_loop_init
-    mov x3 0 
+    MOV x3 0 
 x_loop_init
-    rand x6
-    and x6 1
-    store x6 x4
-    add x4 1
-    loop x_loop_init x3 512 
-    loop y_loop_init x2 256 
-    mov x2 0 
+    RAND x6
+    AND x6 1
+    STORE x6 x4
+    ADD x4 1
+    LOOP x_loop_init x3 512 
+    LOOP y_loop_init x2 256 
+    MOV x2 0 
 main_loop
-    mov x3 x2
-    shl x3 17 
-    mov x4 0 
-    mov x6 x1 
-    add x6 x3 
+    MOV x3 x2
+    SHL x3 17 
+    MOV x4 0 
+    MOV x6 x1 
+    ADD x6 x3 
 y_loop_draw
-    mov x5 0 
-x_loop_draw
-    load x10 x6 
-    movifz x10 x12 0xFF000000 0xFFFFFFFF 
-    putpixel x5 x4 x12
-    add x6 1
-    loop x_loop_draw x5 512 
-    loop y_loop_draw x4 256 
-    flush
-    movifz x2 x7 131072 0 
-    mov x9 x7
-    add x9 x1 
-    mov x8 x3 
-    add x8 x1 
+    MOV x5 0 
+X_LOOP_DRAW
+    LOAD x10 x6 
+    MOVIFZ x10 x12 0xFF000000 0xFFFFFFFF 
+    PUTPIXEL x5 x4 x12
+    ADD x6 1
+    LOOP x_loop_draw x5 512 
+    LOOP y_loop_draw x4 256 
+    FLUSH
+    MOVIFZ x2 x7 131072 0 
+    MOV x9 x7
+    ADD x9 x1 
+    MOV x8 x3 
+    ADD x8 x1 
 y_loop_main
-    mov x5 0 
+    MOV x5 0 
 x_loop_main
-    mov x11 0 
-    mov x10 -1 
+    MOV x11 0 
+    MOV x10 -1 
 dy_loop
-    mov x12 x10 
-    mov x13 -1 
-    add x12 x4 
-    shl x12 9 
-    and x12 130560 
+    MOV x12 x10 
+    MOV x13 -1 
+    ADD x12 x4 
+    SHL x12 9 
+    AND x12 130560 
 dx_loop
-    brz loop_end x13 x10 
-    andd x14 x13 x5 511 
-    thor x14 x12 x3
-    add x14 x1 
-    load x15 x14 
-    add x11 x15 
+    BRZ loop_end x13 x10 
+    ANDD x14 x13 x5 511 
+    THOR x14 x12 x3
+    ADD x14 x1 
+    LOAD x15 x14 
+    ADD x11 x15 
 loop_end
-    loop dx_loop x13 2 
-    loop dy_loop x10 2 
-    breq live x11 3 
-    breq remain x11 2 
-    mov x10 0
-    br update
+    LOOP dx_loop x13 2 
+    LOOP dy_loop x10 2 
+    BREQ live x11 3 
+    BREQ remain x11 2 
+    MOV x10 0
+    BR update
 live
-    mov x10 1
-    br update
+    MOV x10 1
+    BR update
 remain
-    load x10 x8 
+    LOAD x10 x8 
 update
-    store x10 x16 
-    add x8 1
-    add x9 1
-    loop x_loop_main x5 512 
-    loop y_loop_main x4 256 
-    xor x2 1 
-    br main_loop
+    STORE x10 x16 
+    ADD x8 1
+    ADD x9 1
+    LOOP x_loop_main x5 512 
+    LOOP y_loop_main x4 256 
+    XOR x2 1 
+    BR main_loop
